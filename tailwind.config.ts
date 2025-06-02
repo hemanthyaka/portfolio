db307a60f2796c267f023f1a1cbde81a8b1bac30
hemanthyaka <yakahemanth3@gmail.com>
2025-06-02 21:17:42 +0530

    font changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import type { Config } from "tailwindcss";
 
 export default {
@@ -20,7 +19,7 @@ export default {
 		},
 		extend: {
 			fontFamily: {
-				'sans': ['Inter', 'Helvetica Neue', 'Arial', 'sans-serif'],
+				'sans': ['Lato', 'sans-serif'],
 			},
 			colors: {
 				border: 'hsl(var(--border))',
